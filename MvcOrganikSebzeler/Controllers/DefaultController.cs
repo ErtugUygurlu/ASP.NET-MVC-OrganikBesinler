00d1ed394121eca3dca4622b41a46fee4230312a
ErtugUygurlu <ertu_ygrl@hotmail.com>
2023-03-14 17:05:46 +0300

    Add project files.

---
+++ MvcOrganikSebzeler/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOrganikSebzeler.Models.Entity;
+
+namespace MvcOrganikSebzeler.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        ProjeTarimEntities db= new ProjeTarimEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblUrunler.ToList();
+            return View(degerler);
+        }
+    }
+}
